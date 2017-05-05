a8f25421b5fc00d9a7cd69d0f7f78136ea699c6c
hsenid <damitha@hsenidlanka.com>
2017-05-05 07:50:00 +0530

    SpringSecurity

--- src/main/java/com/web/controller/MainController.java
+++ src/main/java/com/web/controller/MainController.java
@@ -5,7 +5,6 @@ import com.translator.Translate;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,86 +12,94 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 
 @Controller
-public class MainController  {
-	Translate translate=new Translate();
-	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-	public ModelAndView defaultPage() {
+public class MainController {
+    Translate translate = new Translate();
 
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Login Form - Database Authentication");
-		model.addObject("message", "This is default page!");
-		model.setViewName("hello");
-		return model;
+    @RequestMapping(value = {"/", "/welcome**"}, method = RequestMethod.GET)
+    public ModelAndView defaultPage() {
 
-	}
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Login Form - Database Authentication");
+        model.addObject("message", "This is default page!");
+        model.setViewName("hello");
+        return model;
 
-	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
-	public ModelAndView adminPage() {
+    }
 
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Login Form - Database Authentication");
-		model.addObject("message", "This page is for ROLE_ADMIN only!");
-		model.setViewName("admin");
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
 
-		return model;
-
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout) {
-
-		ModelAndView model = new ModelAndView();
-		if (error != null ) {
-			model.addObject("error", "Invalid username and password!");
-		}
-
-		if (logout != null) {
-			model.addObject("msg", "You've been logged out successfully.");
-		}
-		model.setViewName("login");
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Login Form - Database Authentication");
+        model.addObject("message", "This page is for ROLE_ADMIN only!");
+        model.setViewName("admin");
 
         return model;
 
-	}
-	
-	//for 403 access denied page
-	@RequestMapping(value = "/403", method = RequestMethod.GET)
-	public ModelAndView accesssDenied() {
+    }
 
-		ModelAndView model = new ModelAndView();
-		
-		//check if user is login
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (!(auth instanceof AnonymousAuthenticationToken)) {
-			UserDetails userDetail = (UserDetails) auth.getPrincipal();
-			System.out.println(userDetail);
-		
-			model.addObject("username", userDetail.getUsername());
-			
-		}
-		
-		model.setViewName("403");
-		return model;
+    @RequestMapping(value = "/hello**", method = RequestMethod.GET)
+    public ModelAndView userPage() {
 
-	}
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Login Form - Database Authentication");
+        model.addObject("message", "This page is for ROLE_USER only!");
+        model.setViewName("hello");
 
-	@RequestMapping(value = "/translator", method = {RequestMethod.POST, RequestMethod.GET})
-	public ModelAndView translatePage(){
-		ModelAndView model = new ModelAndView();
-		Languages languages=new Languages();
-		model.addObject("languages",translate.getLanguages().getDirs());
+        return model;
 
-		model.setViewName("translator");
-		return model;
-	}
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+                              @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
+
+        return model;
+
+    }
+
+    //for 403 access denied page
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public ModelAndView accesssDenied() {
+
+        ModelAndView model = new ModelAndView();
+
+        //check if user is login
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserDetails userDetail = (UserDetails) auth.getPrincipal();
+            System.out.println(userDetail);
+
+            model.addObject("username", userDetail.getUsername());
+
+        }
+
+        model.setViewName("403");
+        return model;
+
+    }
+
+    @RequestMapping(value = "/translator", method = {RequestMethod.POST, RequestMethod.GET})
+    public ModelAndView translatePage() {
+        ModelAndView model = new ModelAndView();
+        Languages languages = new Languages();
+        model.addObject("languages", translate.getLanguages().getDirs());
+
+        model.setViewName("translator");
+        return model;
+    }
 
 
 }
